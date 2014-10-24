c605f976fd166b7221551bac713946e68284e852
fengg5241 <fengg_5241@163.com>
2014-10-24 21:20:25 +0800

    add uploadItem

---
+++ fallinlove/src/main/java/com/shan/fallinlove/model/UploadItem.java
@@ -0,0 +1,30 @@
+package com.shan.fallinlove.model;
+
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+public class UploadItem {
+	private String filename;
+	private CommonsMultipartFile fileData;
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public CommonsMultipartFile getFileData() {
+		return fileData;
+	}
+
+	public void setFileData(CommonsMultipartFile fileData) {
+		this.fileData = fileData;
+	}
+
+	@Override
+	public String toString() {
+		return "UploadItem [filename=" + filename + ", fileData=" + fileData
+				+ "]";
+	}
+}
