809e7d30dd49da198537622a118c62cc1091c13e
fengg5241 <fengg_5241@163.com>
2014-10-27 14:24:18 +0800

    add attribute for LetterDto

--- fallinlove/src/main/java/com/shan/fallinlove/dto/LetterDto.java
+++ fallinlove/src/main/java/com/shan/fallinlove/dto/LetterDto.java
@@ -1,5 +1,37 @@
 package com.shan.fallinlove.dto;
 
+import com.shan.fallinlove.model.Letter;
+import com.shan.fallinlove.model.User;
+
 public class LetterDto {
 
+	private Letter letter;
+	
+	private User fromUser;
+	
+	private User toUser;
+	
+	public Letter getLetter() {
+		return letter;
+	}
+
+	public void setLetter(Letter letter) {
+		this.letter = letter;
+	}
+
+	public User getFromUser() {
+		return fromUser;
+	}
+
+	public void setFromUser(User fromUser) {
+		this.fromUser = fromUser;
+	}
+
+	public User getToUser() {
+		return toUser;
+	}
+
+	public void setToUser(User toUser) {
+		this.toUser = toUser;
+	}
 }
